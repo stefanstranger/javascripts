46c888f2acb861f2651eedf4f461efb1129901fe
Stefan Stranger <stefan@stranger.nl>
2018-08-25 20:31:05 +0200

    Updated javascript

--- Testing/parsejson.js
+++ Testing/parsejson.js
@@ -1,12 +1,32 @@
 var jsonStefan = '[{"firstName": "Stefan", "lastName":"Stranger"}]';    
-var jsonEsther = '[{"firstName": "Esther", "lastName":"Leibbrand"}]';
 
-// Stefan Promise
-var Stefan = function (jsonStefan) {
-    var message = 'My firstname is ' + JSON.parse(jsonStefan)[0].firstName;
+// FirstName Promise
+var firstName = function (param) {
+    var message = JSON.parse(param)[0].firstName;
     return Promise.resolve(message);
 }
 
+// 2nd FirstName Promise
+var secondFirstName = function (message) {
+    var message = 'Your firstname is ' + message;
+    return Promise.resolve(message);
+}
+
+//Call our Promise
+var whoAmI = function () {
+    firstName(jsonStefan)
+    .then((secondFirstName))
+    .then(function (fullfilled){
+        console.log(fullfilled);
+    })
+    .catch(function (error){
+        console.log(error.message);
+    })
+}
+
+whoAmI();
+
+/*
 // Esther Promise
 var Esther = function (jsonEsther) {
     var message = 'My firstname is ' + JSON.parse(jsonEsther)[0].firstName;
@@ -26,3 +46,4 @@ var whoAmI = function () {
 }
 
 whoAmI();
+*/
