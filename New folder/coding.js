97185a856bf9879efeb4d9f475b0cbf24860c89b
Stefan Stranger <stefan@stranger.nl>
2018-08-25 17:19:01 +0200

    some scripts to play around

---
+++ New folder/coding.js
@@ -0,0 +1,10 @@
+console.log('Testing Node.js');
+
+//Create an object
+var myObject = {
+    Firstname: "Stefan",
+    LastName: "Stranger"
+}
+
+//output object
+console.log("My firstname is: " + myObject.Firstname);
